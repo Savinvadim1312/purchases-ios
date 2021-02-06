758b42d1c4836478f8e0b1307dda02582c2bae3a
Vadim Savin <savinvadim1312@gmail.com>
2021-02-06 20:05:09 +0000

    weather screen UI

--- Examples/MagicWeatherReactNative/src/screens/WeatherScreen/index.js
+++ Examples/MagicWeatherReactNative/src/screens/WeatherScreen/index.js
@@ -1,10 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import styles from './styles.js';
 
 const WeatherScreen = (props) => {
+  const currentData = {
+    emoji: '❄️',
+    temperature: 14,
+    unit: 'F',
+    weatherColor: '#043580',
+  };
+
+  const changeEnvironment = () => {
+    /// - we'll change the environment in a future update
+    console.log('Change environment');
+  };
+
+  const performMagic = () => {
+    /*
+     We should check if we can magically change the weather (subscription active) and if not, display the paywall.
+     */
+  };
+
   return (
-    <View>
-      <Text>Weather Screen</Text>
+    <View style={[styles.page, { backgroundColor: currentData.weatherColor }]}>
+      <Text style={styles.emoji}>{currentData.emoji}</Text>
+
+      <Text style={styles.temperature}>
+        {currentData.temperature}°{currentData.unit.toUpperCase()}️
+      </Text>
+
+      <Pressable onPress={changeEnvironment}>
+        <Text style={styles.environment}>
+          <Ionicons name="navigate" color="white" size={24} /> Earth
+        </Text>
+      </Pressable>
+
+      <Pressable onPress={performMagic} style={styles.changeWeatherButton}>
+        <Text style={styles.changeWeatherTitle}>✨ Change the Weather</Text>
+      </Pressable>
     </View>
   );
 };
