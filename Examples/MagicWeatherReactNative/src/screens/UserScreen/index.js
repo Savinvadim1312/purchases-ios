f414ae4a837e289bb55d895f2fc3bf7da8b44787
Vadim Savin <savinvadim1312@gmail.com>
2021-02-06 22:45:49 +0000

    user screen

--- Examples/MagicWeatherReactNative/src/screens/UserScreen/index.js
+++ Examples/MagicWeatherReactNative/src/screens/UserScreen/index.js
@@ -3,13 +3,74 @@
  * @author Vadim Savin
  */
 
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable, TextInput } from 'react-native';
+import styles from './styles.js';
 
-const UserScreen = (props) => {
+const UserScreen = () => {
+  const [newUserId, setNewUserId] = useState('');
+
+  const subscriptionActive = false;
+  const isAnonymous = false;
+
+  const restorePurchases = () => {
+    // restore purchases
+  };
+
+  const login = () => {
+    if (!newUserId) {
+      return;
+    }
+
+    // TODO Implement login
+
+    setNewUserId('');
+  };
+
+  const logout = () => {
+    // TODO implement logout
+  };
+
+  const loginUI = () => (
+    <>
+      <Text style={[styles.headline, { paddingTop: 24 }]}>Login</Text>
+      <TextInput
+        value={newUserId}
+        onChangeText={setNewUserId}
+        onEndEditing={login}
+        placeholder="Enter App User ID"
+        placeholderTextColor="lightgrey"
+        style={styles.input}
+      />
+    </>
+  );
+
+  const logoutUI = () => (
+    <Pressable onPress={logout} style={{ marginTop: 'auto' }}>
+      <Text style={styles.logoutText}>Logout</Text>
+    </Pressable>
+  );
+
   return (
-    <View>
-      <Text>User Screen</Text>
+    <View style={styles.page}>
+      {/* The user's current app user ID and subscription status */}
+      <Text style={styles.headline}>Current User Identifier</Text>
+      <Text style={styles.userIdentifier}>User id</Text>
+
+      <Text style={styles.headline}>Subscription Status</Text>
+      <Text style={{ color: subscriptionActive ? 'green' : 'red' }}>
+        {subscriptionActive ? 'Active' : 'Not Active'}
+      </Text>
+
+      {/* Authentication UI */}
+      {isAnonymous ? loginUI() : logoutUI()}
+
+      {/* You should always give users the option to restore purchases to connect their purchase to their current app user ID */}
+      <Pressable
+        onPress={restorePurchases}
+        style={styles.restorePurchasesButton}>
+        <Text style={styles.restorePurchasesText}>Restore Purchases</Text>
+      </Pressable>
     </View>
   );
 };
---
+++ Examples/MagicWeatherReactNative/src/screens/UserScreen/styles.js
@@ -0,0 +1,43 @@
+/**
+ * @file User Screen styles.
+ * @author Vadim Savin
+ */
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  page: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'space-between',
+    padding: 36,
+  },
+  headline: {
+    color: 'white',
+    fontFamily: 'ArialRoundedMTBold',
+    fontSize: 18,
+    paddingVertical: 8,
+  },
+  userIdentifier: {
+    color: 'white',
+  },
+  input: {
+    paddingTop: 8,
+    color: 'white',
+  },
+  restorePurchasesButton: {
+    marginTop: 'auto',
+  },
+  restorePurchasesText: {
+    color: 'dodgerblue',
+    fontFamily: 'ArialRoundedMTBold',
+    fontSize: 18,
+  },
+  logoutText: {
+    color: 'red',
+    fontFamily: 'ArialRoundedMTBold',
+    fontSize: 18,
+  }
+});
+
+export default styles;
